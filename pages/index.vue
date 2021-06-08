fa1d0d44238fd37872483493026c4e04eb00e432
data-miner00 <data-miner00@users.noreply.github.com>
2021-06-08 22:07:07 +0800

    feat: Working home page

--- pages/index.vue
+++ pages/index.vue
@@ -1,75 +1,157 @@
 <template>
-  <div class="container">
-    <div>
-      <Logo />
-      <h1 class="title">
-        personal-portfolio
-      </h1>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--grey"
-        >
-          GitHub
-        </a>
+  <div class="home">
+    <div class="home__side">
+      <div class="home__logo">
+        <img
+          class="home__logo__img"
+          src="~/assets/images/ck.svg"
+          alt="CK Logo"
+        />
+      </div>
+      <div class="wrapper">
+        <div class="home__links">
+          <div class="home__links__link active">hello</div>
+          <nuxt-link to="/"
+            ><div class="home__links__link">view my profile</div></nuxt-link
+          >
+          <nuxt-link to="/"
+            ><div class="home__links__link">things i do</div></nuxt-link
+          >
+          <nuxt-link to="/"
+            ><div class="home__links__link">get in touch</div></nuxt-link
+          >
+          <nuxt-link to="/"
+            ><div class="home__links__link">testimonial</div></nuxt-link
+          >
+        </div>
+        <div class="home__footer">
+          <div class="home__footer__socials">
+            <div class="home__footer__socials__social">
+              <fa :icon="faFacebookF" />
+            </div>
+            <div class="home__footer__socials__social">
+              <fa :icon="faLinkedinIn" />
+            </div>
+            <div class="home__footer__socials__social">
+              <fa :icon="faGithubAlt" />
+            </div>
+            <div class="home__footer__socials__social">
+              <fa :icon="faTwitter" />
+            </div>
+          </div>
+          <div class="home__footer__craft">
+            crafted with <span>love</span> by terrence.
+          </div>
+        </div>
       </div>
     </div>
+    <div class="home__illustration"></div>
   </div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
+import {
+  faFacebookF,
+  faGithubAlt,
+  faLinkedinIn,
+  faTwitter,
+} from '@fortawesome/free-brands-svg-icons'
 
-export default Vue.extend({})
+export default Vue.extend({
+  head: () => ({
+    title: "Mum Khong's Personal Portfolio",
+    meta: [],
+  }),
+  computed: {
+    faFacebookF() {
+      return faFacebookF
+    },
+    faGithubAlt() {
+      return faGithubAlt
+    },
+    faLinkedinIn() {
+      return faLinkedinIn
+    },
+    faTwitter() {
+      return faTwitter
+    },
+  },
+})
 </script>
 
-<style>
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
+<style lang="sass" scoped>
+.home
+  height: 100vh
 
-.title {
-  font-family:
-    'Quicksand',
-    'Source Sans Pro',
-    -apple-system,
-    BlinkMacSystemFont,
-    'Segoe UI',
-    Roboto,
-    'Helvetica Neue',
-    Arial,
-    sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
+  .wrapper
+    padding-top: 50px
+    padding-left: 20%
+    display: flex
+    flex-direction: column
+    justify-content: space-between
+    height: calc(100vh - 70px)
 
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
+  &__logo
+    height: 70px
+    display: grid
+    place-items: center
+    border-bottom: 1px solid #eee
+    &__img
+      width: 55px
+      display: block
 
-.links {
-  padding-top: 15px;
-}
+  &__links
+    display: flex
+    flex-direction: column
+    row-gap: 20px
+    color: gray
+
+    &__link
+      &.active
+        color: black
+
+  &__footer
+    padding: 20px 0
+
+    &__socials
+      display: flex
+      justify-content: space-between
+      width: 200px
+      margin-bottom: 20px
+
+    &__craft
+      span
+        color: pink
+
+@media screen and (min-width: 620px)
+  .home
+    display: grid
+    grid-template-columns: 230px 1fr
+
+    .wrapper
+      padding-top: 0
+      height: calc(100vh - 157.59px)
+
+    &__logo
+      border-bottom: none
+      place-items: left
+      padding-left: 20%
+      padding-top: 50px
+      padding-bottom: 50px
+      height: fit-content
+      &__img
+        width: 75px
+
+    &__illustration
+      height: 100%
+      background: #eee
+
+    &__footer
+      &__socials
+        width: 150px
+
+@media screen and (min-width: 960px)
+  .home
+    grid-template-columns: 350px 1fr
 </style>
