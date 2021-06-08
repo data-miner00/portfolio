d2f7ca2898488838ef2b39ba585aa98a057e5e68
data-miner00 <data-miner00@users.noreply.github.com>
2021-06-08 22:08:22 +0800

    feat: Detail page layout

---
+++ layouts/detailPage.vue
@@ -0,0 +1,16 @@
+<template lang="pug">
+  .detailPage
+    Header/
+    Nuxt/
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+export default Vue.extend({
+  name: 'detailPage',
+})
+</script>
+
+<style lang="sass" scoped>
+@import "../assets/sass/global"
+</style>
