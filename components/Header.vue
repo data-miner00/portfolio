4b2c8507df5a991ada78a2fe83f6572e786d3105
data-miner00 <data-miner00@users.noreply.github.com>
2021-06-08 22:30:19 +0800

    feat: Completed mobile header

--- components/Header.vue
+++ components/Header.vue
@@ -3,20 +3,19 @@
     .header__dummy
     .header__logo
       img.header__logo__img(src="~/assets/images/ck.svg")
-
-    .header__burger
+    .header__burger(@click="openNav")
       img.header__burger__img(src="~/assets/images/berger.svg")
-    .header__nav-links
-      .header__nav-links__logo
+    .header__nav-links(ref="sidebar")
+      .header__nav-links__logo(@click="closeNav")
         img.header__nav-links__logo__img(src="~/assets/images/ck.svg")
-      NuxtLink(to="/")
-        .header__nav-links__link home
-      NuxtLink(to="/profile")
-        .header__nav-links__link profile
-      NuxtLink(to="/projects")
-        .header__nav-links__link projects
-      NuxtLink(to="/contact")
-        .header__nav-links__link contact
+      NuxtLink(to="/" @click.native="setActive('home')")
+        .header__nav-links__link(:class="{'active': isActive('home')}") home
+      NuxtLink(to="/profile" @click.native="setActive('profile')")
+        .header__nav-links__link(:class="{'active': isActive('profile')}") profile
+      NuxtLink(to="/projects" @click.native="setActive('projects')")
+        .header__nav-links__link(:class="{'active': isActive('projects')}") projects
+      NuxtLink(to="/contact" @click.native="setActive('contact')")
+        .header__nav-links__link(:class="{'active': isActive('contact')}") contact
       .header__nav-links__made-by made with ❤ by terrence #[br] &copy; 2021
 </template>
 
@@ -24,6 +23,23 @@
 import Vue from 'vue'
 export default Vue.extend({
   //
+  data: () => ({
+    selectedPage: 'home',
+  }),
+  methods: {
+    setActive(pageName: string): void {
+      this.selectedPage = pageName
+    },
+    isActive(pageName: string): boolean {
+      return this.selectedPage == pageName
+    },
+    openNav(): void {
+      ;(this.$refs['sidebar'] as HTMLElement).classList.add('pulled-in')
+    },
+    closeNav(): void {
+      ;(this.$refs['sidebar'] as HTMLElement).classList.remove('pulled-in')
+    },
+  },
 })
 </script>
 
@@ -50,7 +66,7 @@ export default Vue.extend({
     height: 100vh
     width: 200px
     background: #eee
-    // transform: translateX(100%)
+    transform: translateX(100%)
     display: flex
     flex-direction: column
     transition: transform .4s ease
@@ -68,6 +84,9 @@ export default Vue.extend({
 
     &__link
       padding: 15px 0
+
+      &.active
+        color: pink
 
     &__made-by
       font-size: 0.5em
