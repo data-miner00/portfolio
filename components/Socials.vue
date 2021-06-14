1bc85ab8cea583e077f7c6da7396565043e8c4c3
data-miner00 <data-miner00@users.noreply.github.com>
2021-06-14 18:50:58 +0800

    feat: Added working links to social component

--- components/Socials.vue
+++ components/Socials.vue
@@ -1,12 +1,12 @@
 <template lang="pug">
   .socials
-    .socials__social
+    a.socials__social(href="https://www.facebook.com/sysdate" target="_blank")
       fa(:icon="faFacebook")
-    .socials__social
+    a.socials__social(href="https://www.linkedin.com/in/chong-mum-khong-343b3b1b0/" target="_blank")
       fa(:icon="faGithub")
-    .socials__social
+    a.socials__social(href="https://github.com/data-miner00" target="_blank")
       fa(:icon="faLinkedin")
-    .socials__social
+    a.socials__social(href="https://twitter.com/Has66771004" target="_blank")
       fa(:icon="faTwitter")
 </template>
 
@@ -44,11 +44,15 @@ export default Vue.extend({
   font-size: 1.65em
   justify-content: space-between
   width: 170px
-  margin: 0 auto
+  margin: 40px auto 0
+  color: gray
+  &__social
+    display: block
 
 @media screen and (min-width: 960px)
   .socials
     &__social
+
       transition: color .4s
       &:hover
         color: brown
