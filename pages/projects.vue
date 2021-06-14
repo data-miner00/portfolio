44f3237519ea06425b0584c34e322c7aef39962e
data-miner00 <data-miner00@users.noreply.github.com>
2021-06-14 18:15:24 +0800

    refactor: Updated content

--- pages/projects.vue
+++ pages/projects.vue
@@ -14,27 +14,28 @@
         <p>
           E-learning Platform for Distanced Collaborative Coding Assignments is
           a web-application aimed to provide a universal platform for students
-          and lecturers to interact with each other effortlessly for
-          coding-based assignments. This is the final year project of my studies
-          at my university.
+          and lecturers to <span>interact</span> with each other effortlessly
+          for <span>coding-based assignments</span>. This is the final year
+          project of my studies at my university.
         </p>
         <p>
-          The project proposal was made to outline and review all the
-          requirements, technology, workflow and design during the initial
+          The <span>project proposal</span> was made to outline and review all
+          the requirements, technology, workflow and design during the initial
           planning phase that tooks about three months and the actual
           implementation of the system takes another three months to complete.
           Although the entire process of developing this system is arduous and
           tiresome, I am flattered that I am rewarded an A- grade for this.
         </p>
         <p>
-          The core feature of this application is the concurrent code editor
-          that allows students to program together in a group in real-time. It
-          also provides a chatting system for the students to discuss by
-          interact with each other with text messages. Besides, this application
-          has a very strict rule on governing the relationships between
-          different entities such as one student can join many course, one
-          course can have many assignments etc. This is extremely important but
-          the most cumbersome part to implement.
+          The core feature of this application is the
+          <span>concurrent code editor</span> that allows students to program
+          together in a group in real-time. It also provides a
+          <span>chatting system</span> for the students to discuss by interact
+          with each other with text messages. Besides, this application has a
+          very strict rule on governing the relationships between different
+          entities such as one student can join many course, one course can have
+          many assignments etc. This is extremely important but the most
+          cumbersome part to implement.
         </p>
       </Project>
       <Project
@@ -44,6 +45,18 @@
         imgSrc="temp.jpg"
         :stacks="stacks[1]"
       >
+        <p>
+          Ignite de Spark is the <span>last event</span> that committees of my
+          society of my batch before we retire and this website was made to
+          <span>promote the event</span> by providing every single bits of
+          details of the event and a call for action to a Google form for
+          registration. This website was the
+          <span>first-ever project</span> that I've done apart from assignments.
+          Hence, the quality are substandard but it was a resounding success for
+          me back then. Unfortunately, due to the unforeseen outbreak of
+          pandemic in our country, the event was canceled and the website was
+          never been to light.
+        </p>
       </Project>
       <Project
         name="Personal Blog"
@@ -52,6 +65,13 @@
         imgSrc="temp.jpg"
         :stacks="stacks[2]"
       >
+        <p>
+          I have started out building the first blogging website that I can
+          write and post at any time I wish. The incentive is to broaden my
+          horizon into topics that are not related to technology while revamping
+          my writing skills in various languages. I have yet to post any useful
+          article at the moment but will do it soon.
+        </p>
       </Project>
       <Project
         name="Personal Portfolio"
@@ -60,8 +80,18 @@
         imgSrc="temp.jpg"
         :stacks="stacks[3]"
       >
+        <p>
+          This project is to build my <span>portfolio website</span>, which is
+          here, where I will be sharing information about me, such as projects
+          and education, basically an elaborate version of the resume. The
+          project is relatively simple but I have still yet to complete it as
+          the crux of this project is not implementation but design. Designing a
+          good website really is tough for me who does not have a multimedia
+          background but I will work through no matter what it takes.
+        </p>
       </Project>
     </div>
+    <Socials />
   </div>
 </template>
 
@@ -167,7 +197,6 @@ export default Vue.extend({
   padding: 50px 0
   &__label
     text-align: center
-    padding-bottom: 40px
     h1
       font-style: italic
       font-size: 1.25em
